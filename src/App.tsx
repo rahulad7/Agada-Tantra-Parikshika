4e019869b4e74e05b177f03049325dd0e54f5b73
GRX10 <grx10@admins-MacBook-Pro.local>
2025-05-24 23:25:17 +0530

    setuped the Agada Tantra Parikshika Project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold justify-center text-blue-700 ">Welcome to Agada Tantra Parikshika</h1>
+    </div>
+  );
+}
+
+export default App;
